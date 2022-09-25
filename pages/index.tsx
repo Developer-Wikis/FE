b3b0e64e129657f6cff981b4451e22c692bd6f82
jinist94 <325days@naver.com>
2022-09-26 03:21:52 +0900

    chore: project setting

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Developer Wiki</title>
+        <meta name="description" content="Developer Wiki" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
