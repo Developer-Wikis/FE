cd099ee776308de7cf72f2e0b042bccc219e6d9a
jinist94 <325days@naver.com>
2022-10-01 11:04:45 +0900

    feat: Pause 아이콘 추가

--- src/components/base/Icon/svg.ts
+++ src/components/base/Icon/svg.ts
@@ -4,3 +4,4 @@ export { default as Microphone } from '~/assets/icons/microphone.svg';
 export { default as Play } from '~/assets/icons/play.svg';
 export { default as Refresh } from '~/assets/icons/refresh.svg';
 export { default as Stop } from '~/assets/icons/stop.svg';
+export { default as Pause } from '~/assets/icons/pause.svg';
