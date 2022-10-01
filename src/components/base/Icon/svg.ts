fab24f5eecd8e0c48028257b7f335438ebc61f72
YuHyun <puh0128@gmail.com>
2022-10-01 09:46:59 +0900

    feat: Icon 컴포넌트 구현

---
+++ src/components/base/Icon/svg.ts
@@ -0,0 +1,6 @@
+export { default as Close } from '~/assets/icons/close.svg';
+export { default as Comment } from '~/assets/icons/comment.svg';
+export { default as Microphone } from '~/assets/icons/microphone.svg';
+export { default as Play } from '~/assets/icons/play.svg';
+export { default as Refresh } from '~/assets/icons/refresh.svg';
+export { default as Stop } from '~/assets/icons/stop.svg';
