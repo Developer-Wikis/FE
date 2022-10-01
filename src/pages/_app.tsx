a2677e48a677395980fc6788567511f9efc6b5f4
jinist94 <325days@naver.com>
2022-10-01 23:42:44 +0900

    feat(questionDetail): 녹음 기능 UI에 맞게 재 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
