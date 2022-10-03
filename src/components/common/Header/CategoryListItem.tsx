10acd3356778fa78e74217dca84454cf72934286
jinist94 <325days@naver.com>
2022-10-04 03:01:09 +0900

    feat: Header 선택된 대분류 표시 및 질문 등록 버튼 style 변경

--- src/components/common/Header/CategoryListItem.tsx
+++ src/components/common/Header/CategoryListItem.tsx
@@ -4,11 +4,12 @@ import styled from '@emotion/styled';
 interface CategoryListItemProps {
   href: string;
   name: string;
+  select: boolean;
 }
 
-const CategoryListItem = ({ href, name }: CategoryListItemProps) => {
+const CategoryListItem = ({ href, name, select }: CategoryListItemProps) => {
   return (
-    <Li>
+    <Li className={select ? 'selected' : ''}>
       <Link href={href}>{name}</Link>
     </Li>
   );
@@ -18,7 +19,13 @@ export default CategoryListItem;
 
 const Li = styled.li`
   margin-right: 16px;
+
   a {
     font-size: 16px;
+  }
+
+  &.selected a {
+    color: ${({ theme }) => theme.colors.red};
+    font-weight: 600;
   }
 `;
