77fd668d87f1cecaec6902b067e5ca9ec4ce8146
jinist94 <325days@naver.com>
2022-10-03 09:08:49 +0900

    feat: Arrow Icon 등록 및 Icon props stroke 추가

--- src/components/base/Icon/index.tsx
+++ src/components/base/Icon/index.tsx
@@ -5,11 +5,20 @@ export interface IconProps {
   name: keyof typeof icons;
   size?: string;
   color?: ThemeColors;
+  stroke?: ThemeColors;
 }
 
-const Icon = ({ name, size = '20', color = 'white', ...props }: IconProps) => {
+const Icon = ({ name, size = '20', color = 'white', stroke = 'white', ...props }: IconProps) => {
   const SvgIcon = icons[name];
-  return <SvgIcon width={size} height={size} color={theme.colors[color]} {...props} />;
+  return (
+    <SvgIcon
+      width={size}
+      height={size}
+      color={theme.colors[color]}
+      stroke={theme.colors[stroke]}
+      {...props}
+    />
+  );
 };
 
 Icon.Button = IconButton;
