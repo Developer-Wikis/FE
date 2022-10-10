187c62a17f5e345be5a0aa304805b7888e798a73
YuHyun <puh0128@gmail.com>
2022-10-11 00:16:42 +0900

    refactor: 불필요한 코드 제거

--- src/utils/helper/formatting.ts
+++ src/utils/helper/formatting.ts
@@ -12,12 +12,3 @@ export const formatDate = (date: string) => {
 export const formatTime = (minutes: number, seconds: number) => {
   return `${minutes}:${seconds < 10 ? 0 : ''}${seconds}`;
 };
-
-export const formatCategory = (mainCategory: string, subCategory: string) => {
-  if (subCategory === '전체') {
-    return mainCategory === 'fe' ? 'FE ALL' : 'BE ALL';
-  }
-
-  // 카테고리 완성되면 카테고리에 포함되지 않은 query입력 시 null 반환하도록 변경
-  return subCategory;
-};
