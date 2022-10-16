c069c0df8ca95ce93bc1560981c7df9fbb390efb
YuHyun <puh0128@gmail.com>
2022-10-16 16:47:57 +0900

    feat: Select 컴포넌트 selected prop 추가

--- src/components/base/select/index.tsx
+++ src/components/base/select/index.tsx
@@ -5,10 +5,11 @@ interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   list: { value: string; text: string }[];
   name?: string;
   onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  selected?: string;
 }
-const Select = ({ list, name, onChange, ...props }: SelectProps) => {
+const Select = ({ list, name, onChange, selected, ...props }: SelectProps) => {
   return (
-    <StyledSelect name={name} id={name} onChange={onChange} {...props}>
+    <StyledSelect name={name} id={name} onChange={onChange} value={selected} {...props}>
       <option value="none">선택해주세요</option>
       {list.map((category, index) => (
         <option key={index} value={category.value}>
--- src/components/domain/random/MainCategoryField.tsx
+++ src/components/domain/random/MainCategoryField.tsx
@@ -6,9 +6,10 @@ import { convertMainCategory } from '~/utils/helper/converter';
 
 interface MainCategoryFieldProps {
   handleChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  selected: string;
 }
 
-const MainCategoryField = ({ handleChange }: MainCategoryFieldProps) => {
+const MainCategoryField = ({ handleChange, selected }: MainCategoryFieldProps) => {
   return (
     <div>
       <Label htmlFor="mainCategory">직무 선택</Label>
@@ -20,6 +21,7 @@ const MainCategoryField = ({ handleChange }: MainCategoryFieldProps) => {
         name="mainCategory"
         style={{ width: '383px' }}
         onChange={handleChange}
+        selected={selected}
       />
     </div>
   );
