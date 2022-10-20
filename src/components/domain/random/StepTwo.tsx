dc51925c745f56b21ff5c9e03ce4502b3e59e860
YuHyun <puh0128@gmail.com>
2022-10-20 19:01:46 +0900

    style: StepTwo 컴포넌트 style 수정
    - title + content 상하 여백 38px로 변경
    - theme.fontStyle 적용
    - 안내사항 li 태그 폰트 크기 14px로 변경
    - 안내사항 li 태그 상하 여백 5px로 변경

--- src/components/domain/random/StepTwo.tsx
+++ src/components/domain/random/StepTwo.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { FormEvent, useEffect, useRef } from 'react';
 import Button from '~/components/base/Button';
 import Icon from '~/components/base/Icon';
+import PageTitle from '~/components/base/PageTitle';
 import { Step } from '~/pages/random/create';
 import { initialPermission } from '~/pages/random/create';
 
@@ -35,7 +36,7 @@ const StepTwo = ({ step, permission, handleSubmit, handleChange, handleBack }: S
           size="36px"
           onClick={handleBack}
         />
-        <Title>안내 사항</Title>
+        <StyledPageTitle>안내 사항</StyledPageTitle>
       </div>
 
       <form onSubmit={handleSubmit}>
@@ -85,17 +86,15 @@ const StepTwo = ({ step, permission, handleSubmit, handleChange, handleBack }: S
 
 export default StepTwo;
 
-const Title = styled.h2`
-  font-size: 24px;
-  text-align: center;
-  margin-bottom: 34px;
+const StyledPageTitle = styled(PageTitle)`
+  margin-bottom: 38px;
 `;
 
 const Subtitle = styled.h3`
   display: block;
   text-align: center;
   margin-bottom: 21px;
-  font-size: 16px;
+  ${({ theme }) => theme.fontStyle.body1}
   font-weight: 600;
   color: ${({ theme }) => theme.colors.gray800};
 `;
@@ -121,14 +120,15 @@ const AudioCheckbox = styled.div`
   margin-bottom: 28px;
 
   input {
-    margin: 0 6px 0 0;
+    margin: 0;
     width: 16px;
     height: 16px;
     accent-color: ${({ theme }) => theme.colors.red};
   }
 
   label {
-    font-size: 14px;
+    margin-left: 6px;
+    ${({ theme }) => theme.fontStyle.body2}
     color: ${({ theme }) => theme.colors.gray600};
   }
 `;
@@ -136,13 +136,13 @@ const AudioCheckbox = styled.div`
 const UL = styled.ul`
   border-radius: 4px;
   margin-bottom: 30px;
-  padding: 26px 23px 26px;
+  padding: 26px 23px;
   background-color: ${({ theme }) => theme.colors.gray200};
 `;
 
 const LI = styled.li`
   margin-left: 16px;
-  font-size: 13px;
+  ${({ theme }) => theme.fontStyle.body2}
   color: ${({ theme }) => theme.colors.gray600};
   list-style: outside;
   word-break: keep-all;
@@ -153,7 +153,7 @@ const LI = styled.li`
   }
 
   ~ li {
-    margin-top: 7px;
+    margin-top: 5px;
   }
 `;
 
