729e5c4d9c7f42acecbaf6aced58ba89694fece4
YuHyun <puh0128@gmail.com>
2022-10-22 23:42:20 +0900

    refactor: BackgroundDim 컴포넌트 분리

---
+++ src/components/base/BackgroundDim/index.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const BackgroundDim = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+`;
+
+export default BackgroundDim;
