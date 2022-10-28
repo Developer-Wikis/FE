d0f5a86c6c73e60e88625b0fed665bb7b3fbebd5
jinist94 <jay@Hamui-MacBook-Pro.local>
2022-10-28 23:09:36 +0900

    feat: PageDescription type props 추가

--- src/components/common/PageDescription/index.tsx
+++ src/components/common/PageDescription/index.tsx
@@ -1,8 +1,29 @@
 import styled from '@emotion/styled';
+import { ReactNode } from 'react';
+import { theme } from '~/types/theme';
 
-const PageDescription = styled.p`
-  color: ${({ theme }) => theme.colors.gray600};
-  ${({ theme }) => theme.fontStyle.body2};
+interface PageDescriptionProps {
+  children: ReactNode;
+  type?: 'normal' | 'bold';
+}
+const PageDescription = ({ children, type = 'normal' }: PageDescriptionProps) => {
+  return <StyledP type={type}>{children}</StyledP>;
+};
+
+const fontStyle = {
+  normal: `
+    color: ${theme.colors.gray600};
+    ${theme.fontStyle.body2}
+  `,
+  bold: `
+  color: ${theme.colors.gray800};
+  ${theme.fontStyle.body1};
+  font-weight: 600;
+  `,
+};
+
+const StyledP = styled.p<PageDescriptionProps>`
+  ${({ type }) => type && fontStyle[type]}
   text-align: center;
   margin-top: 14px;
 `;
