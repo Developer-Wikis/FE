b879ef2f4f530e3702f76498e54730ad33937fc1
jinist94 <jinist94@gmail.com>
2022-11-02 00:46:24 +0900

    fix: AddForm 컴포넌트 재사용을 위해 props 및 내부로직 변경
    - TailQuestionAddForm -> AddForm 으로 이름 변경
    - validation 함수 추가
    - AddForm을 사용하는 TailQuestionField 컴포넌트 코드 수정

--- src/components/common/AddForm/index.tsx
+++ src/components/common/AddForm/index.tsx
@@ -1,19 +1,29 @@
 import styled from '@emotion/styled';
-import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import Button from '~/components/base/Button';
 import Input from '~/components/base/Input';
-import { checkLength, SUBMIT_CHECK } from '~/utils/helper/validation';
 
 interface AddFormProps {
-  type: string;
   name: string;
   id: string;
   buttonText: string;
+  placeholder?: string;
   onSubmit: (value: string) => void;
+  isValid?: (text: string) => boolean;
+  defaultValue?: string;
+  reset?: boolean;
 }
 
-const AddForm = ({ buttonText, onSubmit, ...props }: AddFormProps) => {
-  const [text, setText] = useState('');
+const AddForm = ({
+  buttonText,
+  onSubmit,
+  placeholder,
+  isValid,
+  defaultValue,
+  reset = true,
+  ...props
+}: AddFormProps) => {
+  const [text, setText] = useState(defaultValue ? defaultValue : '');
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -21,23 +31,26 @@ const AddForm = ({ buttonText, onSubmit, ...props }: AddFormProps) => {
     const validText = text.trim();
     setText(validText);
 
-    if (SUBMIT_CHECK.tailQuestion.isValid(text)) {
-      alert(SUBMIT_CHECK.tailQuestion.message);
+    if (isValid && !isValid(validText)) {
       return;
     }
 
     onSubmit(validText);
-    setText('');
+
+    if (reset) {
+      setText('');
+    }
   };
 
   return (
     <Container onSubmit={handleSubmit}>
       <Input
+        type="text"
         onChange={(e: ChangeEvent<HTMLInputElement>) => {
           setText(e.target.value);
         }}
         value={text}
-        placeholder="예상되는 꼬리 질문을 작성해 주세요."
+        placeholder={placeholder}
         {...props}
       />
       <Button size="sm" onClick={handleSubmit}>
