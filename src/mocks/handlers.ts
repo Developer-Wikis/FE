fb48bb847d15f2886390266be94cb8f4fa0d8304
YuHyun <puh0128@gmail.com>
2022-11-15 00:18:59 +0900

    feat: 질문 목록 데이터 가져올 수 있도록 MSW 세팅

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,7 @@
-import { randomData } from './data';
+import { questionData, randomData } from './data';
 import { rest } from 'msw';
+
+const baseURL = process.env.BASE_URL;
 
 export const handlers = [
   rest.get('/random', (req, res, ctx) => {
@@ -8,5 +10,20 @@ export const handlers = [
     console.log('mainCategory:', mainCategory, 'subCategories:', subCategories);
 
     return res(ctx.json(randomData));
+  }),
+  rest.get(`${baseURL}/questions`, (req, res, ctx) => {
+    const PAGE_SIZE = 20;
+
+    const mainCategory = req.url.searchParams.get('mainCategory');
+    const subCategory = req.url.searchParams.get('subCategory');
+    const page = Number(req.url.searchParams.get('page'));
+    console.log('mainCategory:', mainCategory, 'subCategories:', subCategory, 'page: ', page);
+
+    return res(
+      ctx.json({
+        ...questionData,
+        content: questionData.content.slice(page * PAGE_SIZE, (page + 1) * PAGE_SIZE),
+      }),
+    );
   }),
 ];
