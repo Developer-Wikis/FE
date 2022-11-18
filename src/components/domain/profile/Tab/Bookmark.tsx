ead939b32a800e0df27d77bc3331d37f2b371ac8
YuHyun <puh0128@gmail.com>
2022-11-18 22:24:00 +0900

    feat: Bookmark 컴포넌트 구현

---
+++ src/components/domain/profile/Tab/Bookmark.tsx
@@ -0,0 +1,103 @@
+import styled from '@emotion/styled';
+import { ChangeEvent } from 'react';
+import Select from '~/components/base/Select';
+import QuestionList from '~/components/domain/QuestionList';
+import { questionData } from '~/mocks/data';
+import { IQuestionItem } from '~/types/question';
+import { MainType, SubWithAllType } from '~/utils/constant/category';
+import {
+  getMainCategorySelectList,
+  getSubCategoryWithAllSelectList,
+} from '~/utils/helper/categorySelect';
+
+type WithNone<T> = T | 'none';
+export type TSubQuery = { mainCategory: WithNone<MainType>; subCategory: WithNone<SubWithAllType> };
+
+export type TQueryBookmark = {
+  tab: 'question';
+  subQuery: TSubQuery;
+  content: IQuestionItem[];
+  page: number;
+  totalPage: number;
+};
+
+interface BookmarkProps {
+  query: TQueryBookmark;
+  onChange: (name: 'subQuery', value: TSubQuery) => void;
+}
+
+const Bookmark = ({ query, onChange }: BookmarkProps) => {
+  const handleMainCategory = (e: ChangeEvent<HTMLSelectElement>) =>
+    onChange('subQuery', {
+      mainCategory: e.target.value as WithNone<MainType>,
+      subCategory: 'none',
+    });
+  const handleSubCategory = (e: ChangeEvent<HTMLSelectElement>) =>
+    onChange('subQuery', {
+      ...query.subQuery,
+      subCategory: e.target.value as WithNone<SubWithAllType>,
+    });
+
+  return (
+    <>
+      <StyledDiv>
+        <div>
+          <StyledSelect
+            name="mainCategory"
+            list={getMainCategorySelectList()}
+            onChange={handleMainCategory}
+            selected={query.subQuery.mainCategory}
+          />
+          {query.subQuery.mainCategory !== 'none' && (
+            <StyledSelect
+              name="subCategory"
+              list={getSubCategoryWithAllSelectList(query.subQuery.mainCategory)}
+              onChange={handleSubCategory}
+              selected={query.subQuery.subCategory}
+              key={query.subQuery.mainCategory}
+            />
+          )}
+        </div>
+
+        <PageInfo>
+          {query.page + 1}/{query.totalPage} 페이지
+        </PageInfo>
+      </StyledDiv>
+
+      <QuestionList
+        questions={
+          query.content.length === 0
+            ? (questionData.content as unknown as IQuestionItem[])
+            : query.content
+        }
+        currentCategory={{
+          mainCategory: 'fe',
+          subCategory: 'all',
+        }}
+      />
+    </>
+  );
+};
+
+export default Bookmark;
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 19px;
+`;
+
+const StyledSelect = styled(Select)`
+  width: auto;
+  display: inline-block;
+
+  & ~ & {
+    margin-left: 13px;
+  }
+`;
+
+const PageInfo = styled.span`
+  ${({ theme }) => theme.fontStyle.body2}
+  color: ${({ theme }) => theme.colors.gray500};
+`;
