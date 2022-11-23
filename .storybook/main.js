557e460771434fe16cd3d71e514353c706a1ec83
jinist94 <jinist94@gmail.com>
2022-11-24 01:04:56 +0900

    chore: storybook 세팅
    ...
    
    - global css/font 설정
    - svgr 파일 읽을 수 있도록 설정
    - ThemeProvider 설정
    - alias 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
+const resolvePath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -10,10 +14,35 @@ module.exports = {
     builder: '@storybook/builder-webpack5',
   },
   webpackFinal: async (config) => {
+    /* rules */
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.module.rules[config.module.rules.length - 2].test =
+      /\.(ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/;
+    console.log(config.module.rules);
+
+    /* modules */
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'styles'];
+
+    /* alias */
     config.resolve.alias = {
       ...config.resolve.alias,
-      '~/*': path.resolve(__dirname, '../'),
+      // '~/': path.resolve(__dirname, '../src'),
+      // 위 코드 적용 안됨
+
+      '~/assets': path.resolve(__dirname, '../src/assets'),
+      '~/types': path.resolve(__dirname, '../src/types'),
+      '~/components': path.resolve(__dirname, '../src/components'),
+      '~/context': path.resolve(__dirname, '../src/context'),
+      '~/hooks': path.resolve(__dirname, '../src/hooks'),
+      '@emotion/core': resolvePath('node_modules/@emotion/react'),
+      '@emotion/styled': resolvePath('node_modules/@emotion/styled'),
+      'emotion-theming': resolvePath('node_modules/@emotion/react'),
     };
+
     return config;
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
+import { addDecorator } from '@storybook/react';
+import EmotionThemeProvider from './decorators/EmotionThemeProvider';
+import '../src/assets/fonts/font.css';
+import '../src/styles/globals.css';
+import '../src/styles/reset.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Story />
+    </>
+  ),
+];
+
+addDecorator(EmotionThemeProvider);
