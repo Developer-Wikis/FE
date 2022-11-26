bc0a9ba03bf2ab49c5a83b2261a916690c38bfed
jinist94 <325days@naver.com>
2022-11-26 23:04:05 +0900

    feat: useBookmark hook 및 API 추가

--- src/service/bookmark.ts
+++ src/service/bookmark.ts
@@ -4,6 +4,9 @@ const bookmarkApi = {
   bookmark(questionId: number) {
     return auth.post(`/bookmarks/${questionId}`).then((res) => res.data);
   },
+  getBookmark(questionId: number, signal?: AbortSignal) {
+    return auth.get(`/bookmarks/${questionId}`, { signal }).then((res) => res.data);
+  },
 };
 
 export default bookmarkApi;
