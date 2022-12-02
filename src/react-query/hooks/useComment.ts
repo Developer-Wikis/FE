3cdd8fde2e28b858e39b233fb0a99c2220adf436
jinist94 <325days@naver.com>
2022-12-03 00:30:56 +0900

    feat: useAddComment hook 작성

--- src/react-query/hooks/useComment.ts
+++ src/react-query/hooks/useComment.ts
@@ -1,10 +1,9 @@
 import commentApi from '~/service/comment';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { QUERY_KEY } from '../queryKey';
+import { CommentType } from '~/types/comment';
 
 export const useComment = (questionId: number) => {
-  const queryClient = useQueryClient();
-
   const queryFn = () => commentApi.getList(questionId);
 
   const { data: comments = [], refetch: updateComments } = useQuery(
@@ -22,5 +21,25 @@ export const useComment = (questionId: number) => {
   return {
     comments,
     updateComments,
+  };
+};
+
+export const useAddComment = (questionId: number) => {
+  const queryClient = useQueryClient();
+
+  const mutationFn = (payload: CommentType) => commentApi.create({ questionId, payload });
+
+  const { mutateAsync: addComment, isLoading } = useMutation(mutationFn, {
+    onSuccess: () => {
+      queryClient.invalidateQueries([QUERY_KEY.comments]);
+    },
+    onError: () => {
+      alert('댓글 등록에 실패했습니다. 다시 시도해주세요.');
+    },
+  });
+
+  return {
+    addComment,
+    isLoading,
   };
 };
