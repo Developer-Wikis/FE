ecd6438b5d3f5a49696fdbc1b91b9f6ce1480236
YuHyun <puh0128@gmail.com>
2022-12-15 21:42:20 +0900

    fix(Profile): 내부 컴포넌트 유저 타입 변경

--- src/components/domain/profile/UserInfo.tsx
+++ src/components/domain/profile/UserInfo.tsx
@@ -2,16 +2,15 @@ import styled from '@emotion/styled';
 import Link from '~/components/base/Link';
 import UserProfile from '~/components/common/UserProfile';
 import { IUser } from '~/types/user';
-import { Nullable } from '~/types/utilityType';
 
 interface UserInfoProps {
-  user: Nullable<IUser>;
+  user: IUser;
 }
 
 const UserInfo = ({ user, ...props }: UserInfoProps) => {
   return (
     <Container {...props}>
-      <StyledUserProfile profileUrl={user?.profileUrl} text={user?.username ?? ''} fontSize="lg" />
+      <StyledUserProfile profileUrl={user.profileUrl} text={user.username} fontSize="lg" />
       <Link href={`/profile/edit`} variant="borderGray" size="sm">
         회원 정보 수정
       </Link>
