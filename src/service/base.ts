8b38c0100c0ec4701e675d2b39d715d3ae0e19b7
jinist94 <325days@naver.com>
2022-12-20 02:35:20 +0900

    feat: refresh token 적용

--- src/service/base.ts
+++ src/service/base.ts
@@ -1,9 +1,42 @@
 import useStorage from '~/hooks/useStorage';
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
 import { LOCAL_KEY } from '~/utils/constant/user';
 
 const baseURL = `${process.env.BASE_URL}`;
 const storage = useStorage('local');
+
+interface RefreshResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+const getRefreshToken = async () => {
+  try {
+    const refresh = storage.getItem(LOCAL_KEY.refresh);
+    const access = storage.getItem(LOCAL_KEY.token);
+    const { data } = await axios.post<RefreshResponse>(
+      '/refreshToken',
+      {
+        accessToken: access,
+        refreshToken: refresh,
+      },
+      {
+        baseURL: baseURL,
+      },
+    );
+
+    console.log(data, 'data');
+
+    storage.setItem(LOCAL_KEY.token, data.accessToken);
+    storage.setItem(LOCAL_KEY.refresh, data.refreshToken);
+
+    return data.accessToken;
+  } catch (e) {
+    console.log('세션이 만료되었습니다.');
+    storage.removeItem(LOCAL_KEY.token);
+    storage.removeItem(LOCAL_KEY.refresh);
+  }
+};
 
 const authConfig = (instance: AxiosInstance) => {
   instance.interceptors.request.use(
@@ -12,11 +45,34 @@ const authConfig = (instance: AxiosInstance) => {
       config.headers = {
         Authorization: `Bearer ${token}` || '',
       };
+
       return config;
     },
     (error) => Promise.reject(error.response),
   );
 
+  instance.interceptors.response.use(
+    (res) => res,
+    async (error: AxiosError) => {
+      const originalRequest: AxiosRequestConfig = error.config;
+
+      /** refresh 실패할 경우 */
+      if (originalRequest.url === '/refreshToken' || error.response?.status !== 401) {
+        console.log('세션이 만료되었습니다.');
+        return Promise.reject(error.response);
+      }
+
+      const accessToken = await getRefreshToken();
+
+      if (accessToken) {
+        originalRequest.headers!.Authorization = `Bearer ${accessToken}`;
+        // 재요청
+        return axios(originalRequest);
+      }
+
+      // return Promise.reject(error);
+    },
+  );
   return instance;
 };
 
